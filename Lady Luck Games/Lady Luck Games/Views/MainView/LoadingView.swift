3977ea52d86d7f32072c9213365b7ba674a06061
Dias <dias.atudinov@gmail.com>
2025-02-06 17:06:14 +0500

    Splash Screen and Main Menu views add

--- Lady Luck Games/Lady Luck Games/Views/MainView/LoadingView.swift
+++ Lady Luck Games/Lady Luck Games/Views/MainView/LoadingView.swift
@@ -1,18 +1,81 @@
-//
-//  LoadingView.swift
-//  Lady Luck Games
-//
-//  Created by Dias Atudinov on 06.02.2025.
-//
-
 import SwiftUI
 
 struct LoadingView: View {
+    @State private var progress: Double = 0.0
+    @State private var timer: Timer?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            VStack {
+                Spacer()
+                Image(.lady1Position)
+                    .resizable()
+                    .scaledToFit()
+                    
+            }.ignoresSafeArea(edges: .bottom)
+            
+            VStack {
+                Spacer()
+                ZStack {
+                    VStack {
+                        ZStack {
+                            Rectangle()
+                                .padding(.horizontal)
+                                .frame(height: DeviceInfo.shared.deviceType == .pad ? 62:31)
+                                .cornerRadius(20)
+                                .foregroundStyle(.loaderBg)
+                            ProgressView(value: progress, total: 100)
+                                .progressViewStyle(LinearProgressViewStyle())
+                                .cornerRadius(20)
+                                .accentColor(Color.mainRed)
+                                .padding(.horizontal, DeviceInfo.shared.deviceType == .pad ? 12:6)
+                                
+                                .scaleEffect(y: DeviceInfo.shared.deviceType == .pad ? 12.0:6.0, anchor: .center)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .stroke(Color.yellow, lineWidth: 1)
+                                        .frame(height: DeviceInfo.shared.deviceType == .pad ? 62:31)
+                                }
+                                .padding(.horizontal, DeviceInfo.shared.deviceType == .pad ? 12:6)
+                            
+                            Text("Loading...")
+                                .font(.custom(Fonts.regular.rawValue, size: DeviceInfo.shared.deviceType == .pad ? 32:16))
+                                .foregroundStyle(.mainYellow)
+                                .textCase(.uppercase)
+                        }.frame(width: UIScreen.main.bounds.width / 2.5)
+                        
+                    }
+                }
+                .foregroundColor(.black)
+                .padding(.bottom, DeviceInfo.shared.deviceType == .pad ? 50:25)
+            }
+        }.background(
+            Image(.bg)
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+                
+        )
+        .onAppear {
+            startTimer()
+        }
+    }
+    
+    func startTimer() {
+        timer?.invalidate()
+        progress = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+            if progress < 100 {
+                progress += 1
+            } else {
+                timer.invalidate()
+            }
+        }
     }
 }
 
 #Preview {
     LoadingView()
 }
+
--- Lady Luck Games/Lady Luck Games/Views/MainView/MainView.swift
+++ Lady Luck Games/Lady Luck Games/Views/MainView/MainView.swift
@@ -1,15 +1,146 @@
-//
-//  MainView.swift
-//  Lady Luck Games
-//
-//  Created by Dias Atudinov on 06.02.2025.
-//
-
 import SwiftUI
 
 struct MainView: View {
+    @State private var showGames = false
+    @State private var showInfo = false
+    @State private var showSettings = false
+    
+    @StateObject var settingsVM = SettingsViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                Spacer()
+                
+                if geometry.size.width < geometry.size.height {
+                    // Вертикальная ориентация
+                    ZStack {
+                        HStack {
+                            Spacer()
+                            VStack {
+                                Spacer()
+                                
+                                Image(.logo)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 100)
+                                
+                                VStack(spacing: 15) {
+                                    
+                                    Button {
+                                        showGames = true
+                                    } label: {
+                                        TextBg(text: "Games", textSize: DeviceInfo.shared.deviceType == .pad ? 40 : 24)
+                                    }
+                                    
+                                    Button {
+                                        showInfo = true
+                                    } label: {
+                                        TextBg(text: "Info", textSize: DeviceInfo.shared.deviceType == .pad ? 40 : 24)
+                                    }
+                                    
+                                    Button {
+                                        showSettings = true
+                                    } label: {
+                                        TextBg(text: "Settings", textSize: DeviceInfo.shared.deviceType == .pad ? 40 : 24)
+                                    }
+                                    
+                                    
+                                }
+                                VStack {
+                                    Image(.lady1Position)
+                                        .resizable()
+                                        .scaledToFit()
+                                }
+                            }
+                            Spacer()
+                        }
+                    }.ignoresSafeArea(edges: .bottom)
+                } else {
+                    ZStack {
+                        HStack {
+                            VStack {
+                                Spacer()
+                                Image(.lady1Position)
+                                    .resizable()
+                                    .scaledToFit()
+                            }.ignoresSafeArea(edges: [.horizontal, .bottom])
+                        VStack {
+                            VStack(spacing: 15) {
+                                Image(.logo)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 100)
+                                
+                                Button {
+                                    showGames = true
+                                } label: {
+                                    TextBg(text: "Games", textSize: DeviceInfo.shared.deviceType == .pad ? 40 : 24)
+                                }
+                                
+                                
+                                Button {
+                                    showInfo = true
+                                } label: {
+                                    TextBg(text: "Info", textSize: DeviceInfo.shared.deviceType == .pad ? 40 : 24)
+                                }
+                                
+                                
+                                
+                                Button {
+                                    showSettings = true
+                                } label: {
+                                    TextBg(text: "Settings", textSize: DeviceInfo.shared.deviceType == .pad ? 40 : 24)
+                                }
+                                
+                                if DeviceInfo.shared.deviceType == .pad {
+                                    Spacer()
+                                }
+                            }
+                            
+                        }
+                            VStack {
+                                Spacer()
+                                Image(.lady1Position)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .opacity(0)
+                            }
+                        }
+                        
+                    }
+                }
+            }
+            .background(
+                Image(.bg)
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .scaledToFill()
+                
+            )
+//            .onAppear {
+//                if settingsVM.musicEnabled {
+//                    MusicPlayer.shared.playBackgroundMusic()
+//                }
+//            }
+//            .onChange(of: settingsVM.musicEnabled) { enabled in
+//                if enabled {
+//                    MusicPlayer.shared.playBackgroundMusic()
+//                } else {
+//                    MusicPlayer.shared.stopBackgroundMusic()
+//                }
+//            }
+            .fullScreenCover(isPresented: $showGames) {
+            }
+            .fullScreenCover(isPresented: $showInfo) {
+                InfoView()
+            }
+            
+            .fullScreenCover(isPresented: $showSettings) {
+                SettingsView(settings: settingsVM)
+            }
+            
+        }
     }
 }
 
