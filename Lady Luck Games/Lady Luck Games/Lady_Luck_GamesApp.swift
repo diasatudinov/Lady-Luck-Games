caebe6e425b4492bf685bb5be3f69dc2e266a2b5
Dias <dias.atudinov@gmail.com>
2025-02-06 15:43:00 +0500

    first init

---
+++ Lady Luck Games/Lady Luck Games/Lady_Luck_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lady_Luck_GamesApp.swift
+//  Lady Luck Games
+//
+//  Created by Dias Atudinov on 06.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Lady_Luck_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
